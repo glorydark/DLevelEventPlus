b1e78ec74510831b5b2c75e60a94d07f000df5ad
glorydark <1083215364@qq.com>
2023-06-09 22:22:27 +0800

    AdventureSettings are now enabled only if Interact is false

--- src/main/java/glorydark/DLevelEventPlus/CheckTask.java
+++ src/main/java/glorydark/DLevelEventPlus/CheckTask.java
@@ -3,6 +3,8 @@ package glorydark.DLevelEventPlus;
 import cn.nukkit.Server;
 import cn.nukkit.level.Level;
 import cn.nukkit.scheduler.Task;
+import glorydark.DLevelEventPlus.utils.ConfigUtil;
+
 public class CheckTask extends Task {
     @Override
     public void onRun(int i) {
@@ -11,9 +13,19 @@ public class CheckTask extends Task {
                 Boolean bool = MainClass.getLevelBooleanInit(level.getName(), "Player", "Interact");
                 Boolean bool1 = MainClass.getLevelBooleanInit(level.getName(), "Block", "AllowPlaceBlock");
                 Boolean bool2 = MainClass.getLevelBooleanInit(level.getName(), "Block", "AllowBreakBlock");
-                boolean final1 = bool != null && !bool;
-                boolean final2 = bool1 != null && bool2 != null && !bool1 && !bool2;
+                boolean final1 = bool != null && !bool; // 是否不可放置
+                boolean final2 = !final1 &&  bool1 != null && bool2 != null && !bool1 && !bool2;
                 level.getPlayers().values().forEach(player -> {
+                    if(ConfigUtil.isAdmin(player)){
+                        player.setAllowInteract(true);
+                        player.setAllowModifyWorld(true);
+                        return;
+                    }
+                    if(ConfigUtil.isOperator(player, level)){
+                        player.setAllowInteract(true);
+                        player.setAllowModifyWorld(true);
+                        return;
+                    }
                     player.setAllowInteract(!final1);
                     player.setAllowModifyWorld(!final2);
                     player.sendCommandData();
